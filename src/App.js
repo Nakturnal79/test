883413491e40509e525f9f6813f4003902793dd3
nakturnal <nakturnal@rambler.ru>
2018-12-16 10:08:25 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="wrapper">
+            <SayFullName name="Aram" surname="Sahakyan" link="vk.com"/>
+            <SayFullName name="Alen" surname="Simonyan" link="facebook.com"/>
+            <SayFullName name="Vazgen" surname="Xachikyan" link="ok.ru"/>
+        </div>
     );
   }
 }
 
+function SayFullName(props) {
+    return (
+        <div>
+            <h1>My name {props.name}, last name - {props.surname}</h1>
+            <a href={props.link}>Profile</a>
+        </div>
+    )
+}
 export default App;
