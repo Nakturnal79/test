4541b49019c9ebc07ff371ef51d15d8ce4e5a03b
Nakturnal79 <nakturnal@rambler.ru>
2018-12-16 21:13:55 +0400

    some changes

--- src/App.js
+++ src/App.js
@@ -3,28 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components'
+
+import Header from './components/Header';
+import Main from './components/Main';
+import Menu from './components/Menu';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+`
 
 class App extends Component {
   render() {
     return (
-        <div className="wrapper">
-            <SayFullName name="Aram" surname="Sahakyan" link="vk.com"/>
-            <SayFullName name="Alen" surname="Simonyan" link="facebook.com"/>
-            <SayFullName name="Vazgen" surname="Xachiyan" link="ok.ru"/>
+        <div className="App">
+            <HeaderWrapper>
+            <Grid>
+            <Header/>
+            </Grid>
+            </HeaderWrapper>
+            <Menu/>
+            <Main/>
         </div>
     );
   }
 }
 
-function SayFullName(props) {
-    return (
-        <div>
-            <h1>My name {props.name}, last name - {props.surname}</h1>
-            <a href={props.link}>Profile</a>
-        </div>
-    )
-}
+
 export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components'
+
+class Menu extends Component {
+    render() {
+        return (
+            <h1>Menu</h1>
+        )
+    }
+}
+
+export default Menu
